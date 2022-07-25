a0359dcd66c6ea58234381428764baa38a641e40
Mark Viti <+>
2022-07-25 16:17:08 -0400

    July-25 Workd

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,31 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const mainArea = document.querySelector("main")
+
+fetch(TRAINERS_URL)
+.then(res => res.json())
+.then(res => showTrainers(res))
+
+function showTrainers(trainersArray) {
+    trainersArray.forEach(trainer => showTrainer(trainer))
+};
+
+function showTrainer(trainerObj) {
+    const trainerCard = document.createElement("div")
+
+    trainerCard.innerHTML= `
+    <div class="card" data-id="${trainerObj.id}"><p>${trainerObj.name}</p>
+        <button data-trainer-id="${trainerObj.id}">Add Pokemon</button>
+    </div>`
+
+    mainArea.append(trainerCard)
+
+    
+}
+ 
+function showPokemon(trainerObj) {
+    fetch(POKEMONS_URL)
+    .then(res => res.json())
+    .then(showPokeman)
+}
